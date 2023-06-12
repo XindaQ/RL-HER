1bd599dc5f5b8f2b20e445a5699110efe67ac39d
XindaQ <60855181+XindaQ@users.noreply.github.com>
2023-06-12 18:24:50 -0400

    Update main.py
    comments

--- main.py
+++ main.py
@@ -70,45 +70,66 @@ def optimize_model(args, policy_net, target_net, optimizer, memory, device):
 
 
 def train(args):
+    # the main function for the training
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
     # Reset Bit-Flip environment
+    # create the env
     env = BitFlipEnv(size=args.state_size, shaped_reward=args.shaped_reward, dynamic=args.dynamic)
+    # get the state dim and the range of the action (the action_dim = 1)
     actions_num = env.size
     states_dim = env.size
 
     # Statistics
-    steps_done = 0
-    reset_target_cnt = 0
-    episode_durations = []
-    eval_reward_array = []
-    success_ratio_array = []
+    # record the training process data
+    steps_done = 0                          # get the total step
+    reset_target_cnt = 0                    # get the target reset data
+    episode_durations = []                  # get the episode length for the performance of the policy
+    eval_reward_array = []                  # get the reward evaluation
+    success_ratio_array = []                # get the success ratio data
 
     # Experience Replay: memory of available transitions
-    memory = ReplayMemory(100000)
+    # create the replay buffer
+    memory = ReplayMemory(100000)           # set a very larger capacity
 
     # Create DQN models
+    # Build the Q networks for the DQN, and also build the target network for the trick
+    # just a simple two-layer network with relu
+    # also put the model to the devices
+    # the action can estimate the Q-function of all actions what followed by the state: S -> Q(S,A)
+    # notice here, the input is s, and we just assume all the a are option and output Q(s, a) for all action
+    # the input will include the target/goal: (s, g), so we need input_dim = 2*state_dim
+    # the output will be the Q func for all action: Q(s, ai) for all ai, thus output_dim = action_choices
     policy_net = DQN(states_dim * 2, args.hidden_dim, actions_num, dropout_rate=args.dropout).to(device)
     target_net = DQN(states_dim * 2, args.hidden_dim, actions_num, dropout_rate=args.dropout).to(device)
 
     # Create optimizer
+    # get the optimizer for all the trainable parameters, with the learning rate
     optimizer = torch.optim.Adam(policy_net.parameters(), lr=args.alpha)
 
     # Failed episodes buffer for DHER
+    # use the buffer for the failed episodes
     failed_episodes = []
     failed_episodes_size = 10
     failed_episodes_index = 0
 
     # Main loop
+    # run enough episodes
     for i_episode in range(args.episodes):
         # Initialize the environment and state
+        # get the init state and target and reset the envs
         state = torch.tensor(env.reset(), device=device, dtype=torch.float).unsqueeze(0)
         goal = torch.tensor(env.target, device=device, dtype=torch.float).unsqueeze(0)
-
+        
+        # prepare for the rewards and common buffer
         episode_reward = 0
         episode_memory = []
+        # the count here is import from the itertools, the itertools.count() create infinite sequence, (count(start=a, step=b))
+        # it can also return the count start from the start, and then step
+        # similiar to while(True):
         for t in count():
             # Select and perform an action
+            # get the action base on the Q function evaluation
             action = select_action(args, state, goal, actions_num, policy_net, steps_done, device)
             next_state, reward, done = env.step(action.item())
 
@@ -234,41 +255,52 @@ def train(args):
 
 
 if __name__ == '__main__':
+    # the main file for the training, primarily use DQN for training
+    
+    # use the parser
     parser = argparse.ArgumentParser(description=None)
 
-    # Run parameters
-    parser.add_argument('--batch-size', type=int, default=256, help='Batch size to train on')
-    parser.add_argument('--episodes', type=int, default=5000, help='Amount of train episodes to run')
-    parser.add_argument('--state-size', type=int, default=4, help='Size of the environments states')
-    parser.add_argument('--shaped-reward', action="store_true")
+    # Run parameters, training param
+    parser.add_argument('--batch-size', type=int, default=256, help='Batch size to train on')           # batch size of exper clips
+    parser.add_argument('--episodes', type=int, default=5000, help='Amount of train episodes to run')   # eposide limit
+    parser.add_argument('--state-size', type=int, default=4, help='Size of the environments states')    # size of states/game
+    parser.add_argument('--shaped-reward', action="store_true")                                         # shaped reward                 
 
     # Model arguments
-    parser.add_argument('--dropout', type=float, default=0, help='Dropout rate')
-    parser.add_argument('--hidden-dim', type=int, default=500, help='Dimension of the hidden layer')
+    parser.add_argument('--dropout', type=float, default=0, help='Dropout rate')                        # network dropout for overfit
+    parser.add_argument('--hidden-dim', type=int, default=500, help='Dimension of the hidden layer')    # network shape/dim for middle
 
     # Optimizer arguments
-    parser.add_argument('--alpha', type=float, default=0.001, help='Alpha - Learning rate')
-    parser.add_argument('--gamma', type=float, default=0.99, help='Gamma - discount factor')
+    parser.add_argument('--alpha', type=float, default=0.001, help='Alpha - Learning rate')             # learning rate
+    parser.add_argument('--gamma', type=float, default=0.99, help='Gamma - discount factor')            # discount
     parser.add_argument('--target-update', type=int, default=500, help='Number of steps until updating target network')
-    parser.add_argument('--reg-param', type=float, default=0, help='L1 regulatization parameter')
+                                                                                                        # warming step
+    parser.add_argument('--reg-param', type=float, default=0, help='L1 regulatization parameter')       # network regulation for overfit
+   
 
     # Epsilon - Greedy arguments
+    # the epsilon-greedy desicion how we choose the random action or an action from the policy
     parser.add_argument('--eps-start', type=float, default=1.0, help='Starting epsilon - in epsilon greedy method')
     parser.add_argument('--eps-end', type=float, default=0.1,
                         help='Final epsilon - in epsilon greedy method. When epsilon reaches this value it will stay')
+    # this is a little bit large, since the max_step in the env is only 200
     parser.add_argument('--eps-decay', type=int, default=500000,
                         help='Epsilon decay - how many steps until decaying to the final epsilon')
 
     # Hindsight Experience Replay (HER) arguments
+    # the para and flags for the HER, the action=stor_ture set the arg to T when this arg show up, and F when the arg is missing
     parser.add_argument('--HER', action="store_true", help="Use the HER algorithm")
     parser.add_argument('--goals', type=int, default=4, help="Number of goals for the HER algorithm")
 
     # Dynamic Hindsight Experience Replay (DHER) arguments
+    # the para and flag for DHER
     parser.add_argument('--DHER', action="store_true", help="Use the HER algorithm")
     parser.add_argument('--dynamic', action="store_true", help="Use the dynamic mode for the bit flip environment")
 
     args = parser.parse_args()
 
     start_time = time.time()
+    # go to the training pipeline
     train(args)
+    # get the whole training time
     print('Run finished successfully in %s seconds' % round(time.time() - start_time))
